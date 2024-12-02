43ffe6b9de0c119bc4b766d71963bace4c71bb46
Shovin <shovindsouza27@gmail.com>
2024-12-02 20:27:48 +0530

    Moved routes to thier respective controller files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import authrouter from '/routes/auth.route.js';
+import router from './routes/auth.route.js';
 
 const app = express();
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", router);
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
